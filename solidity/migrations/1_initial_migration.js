a872cfae0c3c9adbf36c5991d3ce82136ff86506
김형준 <ninini6630@gmail.com>
2022-03-14 11:07:15 +0900

    Feature:back: Truffle 배포 스크립트

--- solidity/migrations/1_initial_migration.js
+++ solidity/migrations/1_initial_migration.js
@@ -5,11 +5,12 @@
 // };
 
 const MyTicket = artifacts.require("MyTicket");
-//const ShowSchedule = artifacts.require("ShowSchedule");
+const ShowSchedule = artifacts.require("ShowSchedule");
 //const ShowScheduleManager = artifacts.require("ShowScheduleManager");
 
 module.exports = function (deployer) {
   deployer.deploy(MyTicket);
-//  deployer.deploy(ShowSchedule);
+  //uint256 showId, uint256 stageId, uint256 startedAt, uint256 endedAt, uint256 totalMintCount
+  deployer.deploy(ShowSchedule, 1, 2, 1234567890, 9876543210, 50);
 //  deployer.deploy(ShowScheduleManager);
 };
