f82e75a663755c411931ebea97c5764281186a74
김형준 <ninini6630@gmail.com>
2022-03-14 10:56:05 +0900

    Feature:back: MyTicket NFT Contract 프로토타입

--- solidity/contracts/MyTicket.sol
+++ solidity/contracts/MyTicket.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@openzeppelin/contacts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts/drafts/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyTicket is ERC721Full {
+contract MyTicket is ERC721Enumerable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     mapping(uint256 => uint8) _showScheduleIds;
     mapping(uint256 => uint8) _seatIds;
     mapping(uint256 => uint256) _prices;
-    mapping(uint256 => uint256) _minterIds;
+    mapping(uint256 => address) _minterIds;
+
+    mapping(uint256 => string) private _tokenURIs;
     
-    constructor() ERC721Full("MyTicket", "MTKT") public {}
+    constructor() ERC721("MyTicket", "TKT") public {}
 
     function create(address to, string memory tokenURI, uint8 showScheduleId, uint8 seatId, uint256 price) public returns (uint256) {
         _tokenIds.increment();
@@ -29,6 +31,28 @@ contract MyTicket is ERC721Full {
         return newTicketId;
     }
 
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+
+        return super.tokenURI(tokenId);
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
     function _setShowScheduleId(uint256 id, uint8 showScheduleId) private {
         _showScheduleIds[id] = showScheduleId;
     }
@@ -41,7 +65,7 @@ contract MyTicket is ERC721Full {
         _prices[id] = price;
     }
 
-    function _setMinterId(uint256 id, uint256 minterId) private {
+    function _setMinterId(uint256 id, address minterId) private {
         _minterIds[id] = minterId;
     }
 
@@ -57,7 +81,15 @@ contract MyTicket is ERC721Full {
         return _prices[tokenId];
     }
 
-    function MinterId(uint256 tokenId) public view returns (uint256) {
+    function MinterId(uint256 tokenId) public view returns (address) {
         return _minterIds[tokenId];
+    }
+
+    function _burn(uint256 tokenId) internal virtual override {
+        super._burn(tokenId);
+
+        if (bytes(_tokenURIs[tokenId]).length != 0) {
+            delete _tokenURIs[tokenId];
+        }
     }
 }
