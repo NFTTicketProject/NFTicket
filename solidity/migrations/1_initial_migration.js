5f8eb0fb6a61d6b94481d48df9b93296bdbbf16a
김형준 <ninini6630@gmail.com>
2022-03-15 23:34:26 +0900

    Feature:back: ShowSchedule 및 ShowScheduleManager Contract 초안 완성, 배포 스크립트 수정

--- solidity/migrations/1_initial_migration.js
+++ solidity/migrations/1_initial_migration.js
@@ -5,12 +5,9 @@
 // };
 
 const MyTicket = artifacts.require("MyTicket");
-const ShowSchedule = artifacts.require("ShowSchedule");
-//const ShowScheduleManager = artifacts.require("ShowScheduleManager");
+const ShowScheduleManager = artifacts.require("ShowScheduleManager");
 
 module.exports = function (deployer) {
   deployer.deploy(MyTicket);
-  //uint256 showId, uint256 stageId, uint256 startedAt, uint256 endedAt, uint256 totalMintCount
-  deployer.deploy(ShowSchedule, 1, 2, 1234567890, 9876543210, 50);
-//  deployer.deploy(ShowScheduleManager);
+  deployer.deploy(ShowScheduleManager);
 };
