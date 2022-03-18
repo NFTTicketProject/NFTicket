f94d6c2525a4d695833a067d63de50958ca2b8ed
sunminky <suns1502@gmail.com>
2022-03-18 16:06:53 +0900

    S06P22A102-74 #refactoring #comment Feature:backend: 유저 프로필 반환

--- back/app/app/routes/controllers/profile.js
+++ back/app/app/routes/controllers/profile.js
@@ -1,11 +1,13 @@
-const prisma = require("../../utils/prisma")
-const service = require("../../services/profile_service")
+const profile_service = require("../../services/profile_service")
 const express = require('express')
 const router = express.Router()
 
 // 프로필 전체 조회
 router.get('/:walletId', async (req, res, err) => {
-    const result = await service.getProfile(req.params.walletId);
+    const result = await profile_service.getProfile(req.params.walletId);
+
+    if(!result)
+        res.status(404)
 
     res.json(result)
 })
@@ -13,28 +15,44 @@ router.get('/:walletId', async (req, res, err) => {
 // 닉네임 조회
 router.get('/nickname/:walletId', async (req, res, err) => {
     // walletID 와 일치하는 닉네임 반환
-    const result = await service.getNickname(req.params.walletId)
+    const result = await profile_service.getNickname(req.params.walletId)
+
+    if(!result)
+        res.status(404)
+
     res.json(result)
 })
 
 // 가입일 조회
 router.get('/createdat/:walletId', async (req, res, err) => {
     // walletID 와 일치하는 가입일 반환
-    const result = await service.getCreatedAt(req.params.walletId)
+    const result = await profile_service.getCreatedAt(req.params.walletId)
+
+    if(!result)
+        res.status(404)
+
     res.json(result)
 })
 
 // 자기소개 조회
 router.get('/description/:walletId', async (req, res, err) => {
     // walletID 와 일치하는 자기소개 반환
-    const result = await service.getDescription(req.params.walletId)
+    const result = await profile_service.getDescription(req.params.walletId)
+
+    if(!result)
+        res.status(404)
+
     res.json(result)
 })
 
 // 사용자 이미지 조회
 router.get('/imageurl/:walletId', async (req, res, err) => {
     // walletID 와 일치하는 자기소개 반환
-    const result = await service.getImageURL(req.params.walletId)
+    const result = await profile_service.getImageURL(req.params.walletId)
+
+    if(!result)
+        res.status(404)
+
     res.json(result)
 })
 
