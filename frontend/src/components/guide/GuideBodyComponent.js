d33ad0bd7c6cd0ba032e5b3d0fa1b3d1ed8003d3
김한길 <nuclear0789@naver.com>
2022-03-23 17:48:02 +0900

    S06P22A102-128 #comment Feature:front: GuideBody 컴포넌트

--- frontend/src/components/guide/GuideBodyComponent.js
+++ frontend/src/components/guide/GuideBodyComponent.js
@@ -2,17 +2,27 @@ import React from "react";
 import "./GuideBodyComponent.css";
 
 // icon components from MUI
-import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
 import SellIcon from "@mui/icons-material/Sell";
+import BrushIcon from "@mui/icons-material/Brush";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import PeopleIcon from "@mui/icons-material/People";
+
+import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
 
 const GuideBodyComponent = (props) => {
   return (
     <div className='bodyComponent'>
       <div className='icon'>
+        {props.icon === "SellIcon" && <SellIcon fontSize='large' />}
+        {props.icon === "BrushIcon" && <BrushIcon fontSize='large' />}
+        {props.icon === "ShoppingBagIcon" && (
+          <ShoppingBagIcon fontSize='large' />
+        )}
+        {props.icon === "PeopleIcon" && <PeopleIcon fontSize='large' />}
+
         {props.icon === "ConfirmationNumberIcon" && (
           <ConfirmationNumberIcon fontSize='large' />
         )}
-        {props.icon === "SellIcon" && <SellIcon fontSize='large' />}
       </div>
       <div className='smallTitle'>{props.title}</div>
       <div className='smallBody'>{props.body}</div>
