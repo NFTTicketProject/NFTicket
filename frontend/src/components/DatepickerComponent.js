0f68ed8cff7d2f09ef41602844f7fc1d0ff3c4a1
ruwan9 <ruwan9@kakao.com>
2022-03-24 10:47:27 +0900

    S06P22A102-94 #comment Feature:front: 날짜 관련 컴포넌트

---
+++ frontend/src/components/DatepickerComponent.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+// CSS Modules, react-datepicker-cssmodules.css
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+const DatepickerComponent = ({ detailInfo, setDetailInfo }) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  // console.log(startDate);
+  // console.log(endDate);
+
+  // 변경사항 저장해서 넘겨주기
+  useEffect(() => {
+    setDetailInfo({ ...detailInfo, startDate: startDate, endDate: endDate });
+  }, [startDate, endDate]);
+
+  return (
+    <>
+      From:
+      <DatePicker
+        selected={startDate}
+        onChange={(date: Date) => setStartDate(date)}
+        showTimeSelect // 시간 나오게 하기
+        timeFormat="HH:mm" //시간 포맷
+        timeIntervals={15} // 15분 단위로 선택 가능한 box가 나옴
+        timeCaption="time"
+        dateFormat="MMMM d, yyyy h:mm aa"
+      />
+      To:
+      <DatePicker
+        selected={endDate}
+        onChange={(date: Date) => setEndDate(date)}
+        showTimeSelect // 시간 나오게 하기
+        timeFormat="HH:mm" //시간 포맷
+        timeIntervals={15} // 15분 단위로 선택 가능한 box가 나옴
+        timeCaption="time"
+        dateFormat="MMMM d, yyyy h:mm aa"
+      />
+    </>
+  );
+};
+
+export default DatepickerComponent;
