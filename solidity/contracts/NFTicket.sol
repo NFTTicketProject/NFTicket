0923a2a34cb931c28a97abc865ed95d887e0ec1b
김형준 <ninini6630@gmail.com>
2022-03-24 17:57:34 +0900

    Feature:back: NFTicket Contract 완성

--- solidity/contracts/NFTicket.sol
+++ solidity/contracts/NFTicket.sol
@@ -1,16 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./SsafyToken.sol";
 import "./MyTicket.sol";
 import "./ShowScheduleManager.sol";
+import "./TicketSaleManager.sol";
 
 contract NFTicket {
+    SsafyToken private _token;
     MyTicket private _myTicket;
     ShowScheduleManager private _showScheduleManager;
+    TicketSaleManager private _ticketSaleManager;
 
-    constructor() {
-        _myTicket = new MyTicket();
-        _showScheduleManager = new ShowScheduleManager(getAddressOfMyTicket());
+    constructor(address tokenContractAddress, address ticketContractAddress) {
+        _token = SsafyToken(tokenContractAddress);
+        _myTicket = MyTicket(ticketContractAddress);
+        _showScheduleManager = new ShowScheduleManager(tokenContractAddress, ticketContractAddress);
+        _ticketSaleManager = new TicketSaleManager(tokenContractAddress, ticketContractAddress);
+    }
+
+    function getAddressOfToken() public returns(address) {
+        return address(_token);
     }
 
     function getAddressOfMyTicket() public returns(address) {
@@ -19,5 +29,9 @@ contract NFTicket {
 
     function getAddressOfShowScheduleManager() public returns(address) {
         return address(_showScheduleManager);
+    }
+
+    function getAddressOfTicketSaleManager() public returns(address) {
+        return address(_ticketSaleManager);
     }
 }
