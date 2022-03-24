dc7997d948159f45c805e7328dacdbd11bc7c601
김형준 <ninini6630@gmail.com>
2022-03-24 23:41:50 +0900

    Feature:back: MyTicket Contract 수정 (msg.sender로 발행처 고정, IssuePrice 삭제, 메소드 순서 변경 등)

--- solidity/contracts/MyTicket.sol
+++ solidity/contracts/MyTicket.sol
@@ -17,27 +17,27 @@ contract MyTicket is ERC721Enumerable {
     }
 
     mapping(uint256 => uint256) _showScheduleIds;
-    mapping(uint256 => uint64) _classIds;
+    mapping(uint256 => uint256) _classIds;
     mapping(uint256 => address) _minterIds;
     mapping(uint256 => ResellPolicy) _resellPolicies;   // map tokenId to ReselPolicy
     mapping(uint256 => uint256) _issuePrices;
     
     constructor() ERC721("MyTicket", "TKT") public {}
 
-    function create(address to, string memory ticketURI, uint256 showScheduleId, uint64 classId, uint256 issuePrice, bool isResellAvailable, uint8 resellRoyaltyRatePercent, uint256 resellPriceLimit) public returns (uint256) {
-        require(to != address(0), "Receiving address cannot to be 0x0");
+    function create(string memory ticketURI, uint256 showScheduleId, uint256 classId, bool isResellAvailable, uint8 resellRoyaltyRatePercent, uint256 resellPriceLimit) public returns (uint256) {
         require(resellRoyaltyRatePercent <= 100, "The resellRoyaltyRatePercent should be smaller than 100");
         require(resellPriceLimit > 0, "The resellPriceLimit should be larger than 0");
         _tokenIds.increment();
 
         uint256 newTokenId = _tokenIds.current();
-        _mint(to, newTokenId);
+        _mint(msg.sender, newTokenId);
+        _setMinterId(newTokenId, msg.sender);
         _setTokenURI(newTokenId, ticketURI);
         _setShowScheduleId(newTokenId, showScheduleId);
         _setClassId(newTokenId, classId);
-        _setIssuePrice(newTokenId, issuePrice);
-        _setMinterId(newTokenId, msg.sender);
         _setResellPolicy(newTokenId, isResellAvailable, resellRoyaltyRatePercent, resellPriceLimit);
+
+        return newTokenId;
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
@@ -57,6 +57,10 @@ contract MyTicket is ERC721Enumerable {
         return super.tokenURI(tokenId);
     }
 
+    function _setMinterId(uint256 tokenId, address minterId) private {
+        _minterIds[tokenId] = minterId;
+    }
+
     function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
         require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
         _tokenURIs[tokenId] = _tokenURI;
@@ -66,12 +70,8 @@ contract MyTicket is ERC721Enumerable {
         _showScheduleIds[tokenId] = showScheduleId;
     }
 
-    function _setClassId(uint256 tokenId, uint64 classId) private {
+    function _setClassId(uint256 tokenId, uint256 classId) private {
         _classIds[tokenId] = classId;
-    }
-
-    function _setMinterId(uint256 tokenId, address minterId) private {
-        _minterIds[tokenId] = minterId;
     }
 
     function _setResellPolicy(uint256 tokenId, bool isResellAvailable, uint8 resellRoyaltyPercent, uint256 resellPriceLimit) private {
@@ -82,8 +82,8 @@ contract MyTicket is ERC721Enumerable {
             });
     }
 
-    function _setIssuePrice(uint256 tokenId, uint256 issuePrice) private {
-        _issuePrices[tokenId] = issuePrice;
+    function MinterId(uint256 tokenId) public view returns (address) {
+        return _minterIds[tokenId];
     }
 
     function TokenURI(uint256 tokenId) public view returns (string memory) {
@@ -94,16 +94,8 @@ contract MyTicket is ERC721Enumerable {
         return _showScheduleIds[tokenId];
     }
 
-    function ClassId(uint256 tokenId) public view returns (uint64) {
+    function ClassId(uint256 tokenId) public view returns (uint256) {
         return _classIds[tokenId];
-    }
-
-    function MinterId(uint256 tokenId) public view returns (address) {
-        return _minterIds[tokenId];
-    }
-
-    function IssuePrice(uint256 tokenId) public view returns (uint256) {
-        return _issuePrices[tokenId];
     }
 
     function _burn(uint256 tokenId) internal virtual override {
@@ -117,9 +109,6 @@ contract MyTicket is ERC721Enumerable {
         }
         if (_classIds[tokenId] != 0) {
             delete _classIds[tokenId];
-        }
-        if (_issuePrices[tokenId] != 0) {
-            delete _issuePrices[tokenId];
         }
         if (_minterIds[tokenId] != address(0)) {
             delete _minterIds[tokenId];
