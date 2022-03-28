044860a3a36177e51a127d68de7d81a802e0ed00
k-min9 <mingu4969@gmail.com>
2022-03-29 00:19:57 +0900

    Feature:front: 이미지에디터 초기로딩

--- frontend/src/pages/Page3.js
+++ frontend/src/pages/Page3.js
@@ -3,44 +3,12 @@ import React, { useRef, useState, useEffect } from "react";
 import "tui-image-editor/dist/tui-image-editor.css";
 import ImageEditor from "@toast-ui/react-image-editor";
 import stamp1 from "../images/stamp/stamp1.png";
-import stamp2 from "../images/stamp/stamp2.png";
-import stamp3 from "../images/stamp/stamp3.png";
-import stamp4 from "../images/stamp/stamp4.png";
-import stamp5 from "../images/stamp/stamp5.png";
-import stamp6 from "../images/stamp/stamp6.png";
-import stamp7 from "../images/stamp/stamp7.png";
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { Container, Grid } from "@mui/material";
 import Button from '@mui/material/Button';
-import { styled, useTheme } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import MenuIcon from '@mui/icons-material/Menu';
 
 import DrawerMain from "../components/toast/DrawerMain.js";
 import { useSearchParams } from "react-router-dom";
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
-}));
-
 
 // 주소 있을 경우 : http://localhost:3000/Toast%20UI?ticket=https://ipfs.io/ipfs/QmUQee4Cd3ECfsji4z4h46xQqoEUirJM3UiK6qCZCNjKYV
 // 주소 없을 경우 : http://localhost:3000/Toast%20UI
@@ -49,18 +17,9 @@ const Page3 = () => {
   const [imagePath, SetImagePath] = useState("");
   const [imageSavedPath, SetImageSavedPath] = useState('');
 
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
   const myTheme = {};
-  const imageSERVER = "https://cdn.filestackcontent.com/AM9o5lgXYR1uvQX2NaAqnz/output=secure:true/"
   const editorRef = useRef(null);
-  const FileSaver = require('file-saver')
-
-  // setState 관련 임시 함수
-  // const onClick = (event) => {
-  //   SetImagePath(event.target.value)
-  // };
+  const FileSaver = require('file-saver')  // 이거 있어야 다운로드 작동함
 
   const ipfsClient = require('ipfs-http-client')
   // const ipfs = ipfsClient.create({ host: 'j6a102.p.ssafy.io/ipfs', port: '443', protocol: 'https' }) 
@@ -75,13 +34,20 @@ const Page3 = () => {
     } else {
       ticketPath = "https://ipfs.io/ipfs/QmXRhj2jj3TJFHMq5C1qsxskcbTwdtAb1LSgeL6neT6MP8"
     }
-    editorRef.current.getInstance().loadImageFromURL(ticketPath, 'newTicket')
+    editorRef.current.getInstance().loadImageFromURL(ticketPath, 'newTicket').then(() => {
+      console.log("A")
+      // editorRef.current.getInstance().redo();
+      // editorRef.current.getInstance().resetFlip();
+      // editorRef.current.getInstance().startDrawingMode('FREE_DRAWING');
+      // editorRef.current.getInstance().clearObjects();
+    })
+    // editorRef.current.getInstance().changeSelectableAll(false);
+    // editorRef.current.getInstance().clearObjects();
+    console.log("B")
   }, [])
 
-
-  const editorAddImage = () => {editorRef.current.getInstance().addImageObject(imageSERVER + 'https://docs.unity3d.com/uploads/Main/ShadowIntro.png');}
   // 이미지 조절 관련 이슈로 질문 올린 상태 : https://github.com/nhn/tui.image-editor/issues/741
-  const editorAddImage2 = () => {editorRef.current.getInstance().addImageObject(stamp1)
+  const editorAddImage = () => {editorRef.current.getInstance().addImageObject(stamp1)
     .then( res => {
       const editorInstance = editorRef.current.getInstance();
       var canvasSize = editorInstance.getCanvasSize();
@@ -90,18 +56,7 @@ const Page3 = () => {
     );}
 
   const editorImageChange = () => {editorRef.current.getInstance().loadImageFromURL(imagePath, 'newTicket')}
-  
-  function editorAddStamp(stamp) {editorRef.current.getInstance().addImageObject(stamp);}
-
-  const itemData = [
-    {img: stamp1, title:'no'},
-    {img: stamp2, title:'no'},
-    {img: stamp3, title:'no'},
-    {img: stamp4, title:'no'},
-    {img: stamp5, title:'no'},
-    {img: stamp6, title:'no'},
-    {img: stamp7, title:'no'},
-  ]
+  function editorAddStamp(stamp) {editorRef.current.getInstance().addImageObject(stamp);}  // 상속 함수
 
   const editorToBase64 = async () => {
     const editorInstance = editorRef.current.getInstance();
@@ -138,25 +93,6 @@ const Page3 = () => {
     return null;
   }
 
-  const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-      flexGrow: 1,
-      padding: theme.spacing(3),
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      marginLeft: `-${240}px`,
-      ...(open && {
-        transition: theme.transitions.create('margin', {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-      }),
-    }),
-  );
-
   return(
     <Grid container spacing={2}>
       <Grid item xs = {2}>
@@ -173,11 +109,11 @@ const Page3 = () => {
           <ImageEditor
           ref={editorRef}
           includeUI={{
-            // 기본사진
-            // loadImage: {
-            //   path: imagePath,
-            //   name: "SampleImage",
-            // },
+            // 기본사진 (최초 로딩 1회 있어야 작동 시작함)
+            loadImage: {
+              path: "https://ipfs.io/ipfs/QmXRhj2jj3TJFHMq5C1qsxskcbTwdtAb1LSgeL6neT6MP8",
+              name: "SampleImage",
+            },
             theme: myTheme,
             menu: [
               // 넣고싶은 기능을 이 배열에서 없애거나 추가할 수 있습니다.
