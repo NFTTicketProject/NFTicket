f1c657823b3aba411f2317b498cce62baccaa91f
선민기 <suns1502@gmail.com>
2022-03-28 11:39:49 +0900

    S06P22A102-61 Feature:back: Add Authorization
    사용자 인증 구현
    프로필 주인만 프로필 내용 변경 가능

---
+++ back/app/app/services/authorization.js
@@ -0,0 +1,16 @@
+const eth = require('ethers')
+const crypto = require('crypto')
+
+module.exports = {
+    ownerCheck : async (param, verkey)=>{
+        if (!param.info.timestamp)
+            return false
+
+        if (Math.abs(new Date(param.info.timestamp) - new Date()) > 600000)
+            return false
+
+        const hash = crypto.createHash('sha256').update(JSON.stringify(param.info)).digest('hex');
+
+        return verkey == eth.utils.verifyMessage(hash, param.sign)
+    },
+}
