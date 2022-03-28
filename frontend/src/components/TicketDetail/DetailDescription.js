027a8f57eed3ad0b9b482d28355dace0421fea29
김한길 <nuclear0789@naver.com>
2022-03-28 16:43:56 +0900

    S06P22A102-122 #comment Feature:front DetailDescription 컴포넌트

---
+++ frontend/src/components/TicketDetail/DetailDescription.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const DetailDescription = () => {
+  const PosterDetail = styled.img``;
+
+  // src='images/posterImg1.png'
+  return (
+    <div>
+      <PosterDetail src='images/AIDA.jpg' alt='poster img'></PosterDetail>
+    </div>
+  );
+};
+
+export default DetailDescription;
