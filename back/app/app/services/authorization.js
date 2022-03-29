d7fedc9b41eed0f0bf5ed61f4fa7a583a159a123
선민기 <suns1502@gmail.com>
2022-03-29 13:59:59 +0900

    Feature:back: Add  Log mesaage
    로그 메세지 추가

--- back/app/app/services/authorization.js
+++ back/app/app/services/authorization.js
@@ -1,13 +1,29 @@
 const eth = require('ethers')
+const {logger} = require('../utils/winston')
 
 module.exports = {
-    ownerCheck : async (param, verkey)=>{
-        if (!param.info.timestamp)
-            return false
+    ownerCheck: async (param, verkey) => {
+        if (!param.info.timestamp) {
+            logger.info('[authorization.js] param.info.timestamp is not exist')
+            return {
+                success : false,
+                message : 'param.info.timestamp is not exist',
+            }
+        }
 
-        if (Math.abs(new Date(param.info.timestamp) - new Date()) > 300000)
-            return false
+        if (Math.abs(new Date(param.info.timestamp) - new Date()) > 300000) {
+            logger.info('[authorization.js] time synchronization needed')
+            return {
+                success : false,
+                message : 'time synchronization needed',
+            }
+        }
 
-        return verkey == eth.utils.verifyMessage(JSON.stringify(param.info), param.hash_sign)
+        const result = verkey === eth.utils.verifyMessage(JSON.stringify(param.info), param.hash_sign)
+
+        return {
+            success : result,
+            message : `signature is ${result?'valid':'invalid'}`,
+        }
     },
 }
