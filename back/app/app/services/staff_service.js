0bebaac92db83482644205a457c75d196da691a7
김형준 <ninini6630@gmail.com>
2022-03-29 18:36:30 +0900

    Feature:back: Staff Service 및 Controller 초안 작성
    Co-authored-by: sunminky <suns1502@gmail.com>

---
+++ back/app/app/services/staff_service.js
@@ -0,0 +1,139 @@
+const prisma = require("../utils/prisma")
+const { logger } = require('../utils/winston')
+
+module.exports = {
+    createStaff : async (info) =>{
+        await prisma.Staff.create({
+            data: info,
+        })
+
+        logger.info('[staff_service.js] createStaff ::: ' + JSON.stringify(info))
+    },
+    setStaff : async (info) =>{
+        try {
+            await prisma.staff.update({
+                where: {
+                    staff_id: info['staff_id'],
+                },
+                data: {
+                    name: info['name'],
+                    imageURL: info['image_uri'],                    
+                },
+            })
+
+            logger.error('[staff_service.js] setStaff ::: ' + JSON.stringify(info))
+
+            return 200
+        } catch (e) {
+            logger.error('[staff_service.js] setStaff ::: ' + e)
+
+            return 500
+        }
+    },
+    setName : async (info) =>{
+        try {
+            await prisma.Staff.update({
+                where: {
+                    staff_id: info['staff_id'],
+                },
+                data: {
+                    name: info['name'],
+                },
+            })
+
+            logger.error('[staff_service.js] setDescription ::: ' + JSON.stringify(info))
+
+            return 200
+        } catch (e) {
+            logger.error('[staff_service.js] setDescription ::: ' + e)
+
+            return 500
+        }
+    },
+    setImageURI : async (info) =>{
+        try {
+            await prisma.Staff.update({
+                where: {
+                    staff_id: info['staff_id'],
+                },
+                data: {
+                    imageURL: info['image_uri'],
+                },
+            })
+
+            logger.error('[staff_service.js] setImageURL ::: ' + JSON.stringify(info))
+
+            return 200
+        } catch (e) {
+            logger.error('[staff_service.js] setImageURL ::: ' + e)
+
+            return 500
+        }
+    },
+    getStaff : async (staffId) =>{
+        const result = await prisma.Staff.findUnique({
+            where: {
+                staff_id: Number(staffId)
+            },
+            select: {
+                staff_id: true,
+                name: true,
+                imageURL: true
+            },
+        })
+
+        result.forEach(el => {
+            logger.info('[staff_service.js] getStaff ::: ' + JSON.stringify(el))
+        });
+
+        return result
+    },
+    getName : async (staffId) =>{
+        const result = await prisma.Staff.findUnique({
+            where: {
+                staff_id: Number(staffId)
+            },
+            select: {
+                name: true,
+            },
+        })
+
+        result.forEach(el => {
+            logger.info('[staff_service.js] getName ::: ' + JSON.stringify(el))
+        });
+
+        return result
+    },
+    getImageURI : async (staffId) =>{
+        const result = await prisma.Staff.findUnique({
+            where: {
+                staff_id: Number(staffId)
+            },
+            select: {
+                imageURL: true,
+            },
+        })
+
+        result.forEach(el => {
+            logger.info('[staff_service.js] getImageURI ::: ' + JSON.stringify(el))
+        });
+
+        return result
+    },
+    // getImageURIByStaffId : async (staffId) =>{
+    //     const result = await prisma.Ticket_images.findMany({
+    //         where: {
+    //             staff_id: Number(staffId)
+    //         },
+    //         select: {
+    //             uri: true,
+    //         },
+    //     })
+
+    //     result.forEach(el => {
+    //         logger.info('[staff_service.js] getImageURIByStaffId ::: ' + JSON.stringify(el))
+    //     });
+
+    //     return result
+    // },
+}
