536bdc6d969e11dac90b475160e3901133e723a2
김형준 <ninini6630@gmail.com>
2022-03-29 14:16:20 +0900

    Feature:back: ShowSchedule 설계 변화 관련 연동부 로직 수정

--- solidity/contracts/TicketSale.sol
+++ solidity/contracts/TicketSale.sol
@@ -5,7 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 import "./MyTicket.sol";
+import "./ShowSchedule.sol";
 import "./ShowScheduleManager.sol";
 
 contract TicketSale is Ownable, IERC721Receiver {
@@ -58,36 +60,43 @@ contract TicketSale is Ownable, IERC721Receiver {
         return _currencyContract.balanceOf(address(this));
     }
 
-    function purchase() public payable ActiveSale {        
+    function purchase() public payable ActiveSale {
         uint256 showScheduleId = _ticketContract.getShowScheduleId(_ticketId);
+        address showScheduleAddr = _showScheduleManagerContract.getShowSchedule(showScheduleId);
         uint256 classId = _ticketContract.getClassId(_ticketId);
-        uint256 classPrice = _showScheduleManagerContract.getTicketClassPrice(showScheduleId, classId);
+        uint256 classPrice = ShowSchedule(showScheduleAddr).getTicketClassPrice(classId);
 
         // 판매자가 티켓 주인인가 확인
-        require(_ticketContract.ownerOf(_ticketId) == _seller);
+        require(_ticketContract.ownerOf(_ticketId) == _seller, "seller is not owner");
 
         // 구매자에게 현재 잔고가 있는가 확인
-        require(_currencyContract.balanceOf(msg.sender) >= classPrice);
+        require(_currencyContract.balanceOf(msg.sender) >= classPrice, "balance is exhausted");
 
         // 판매자에게서 구매자에게 티켓 전송
-        _ticketContract.transferFrom(_seller, msg.sender, _ticketId);
+        _ticketContract.safeTransferFrom(_seller, msg.sender, _ticketId);
 
         // 구매자에게서 CA로 토큰 지불
         _currencyContract.transferFrom(msg.sender, address(this), classPrice);
 
+        // 판매 종료 처리
         end();
     }
 
     function withdraw() public payable EndedSale onlySeller {
         uint256 showScheduleId = _ticketContract.getShowScheduleId(_ticketId);
-        (, uint8 royaltyRatePercent, ) = _showScheduleManagerContract.getResellPolicy(showScheduleId);
+        address showScheduleAddr = _showScheduleManagerContract.getShowSchedule(showScheduleId);
+        (, uint8 royaltyRatePercent, ) = ShowSchedule(showScheduleAddr).getResellPolicy();
 
         uint256 contractBalance = _currencyContract.balanceOf(address(this));
         uint256 ownerAmount = contractBalance.mul(royaltyRatePercent).div(100);
         uint256 sellerAmount = contractBalance.sub(ownerAmount);
+        
+        // 판매자에게 로열티를 제외한 만큼의 토큰 전송
         _currencyContract.transfer(_seller, sellerAmount);
         
+        // 계약주소의 잔고 확인
         uint256 contractBalanceAfterTransfer = _currencyContract.balanceOf(address(this));
+        // 계약주소로부터 계약 소유자(판매 관리 계약 주소)로 로열티를 전송
         _currencyContract.transfer(owner(), contractBalanceAfterTransfer);
     }
 
