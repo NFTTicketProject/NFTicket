2f29bd39d525f1b23c5331ea70723aa4db52e230
김형준 <ninini6630@gmail.com>
2022-03-29 18:30:43 +0900

    Chore: Auth Service 파일명 변경

--- back/app/app/services/auth_service.js
+++ back/app/app/services/auth_service.js
@@ -4,7 +4,7 @@ const {logger} = require('../utils/winston')
 module.exports = {
     ownerCheck: async (param, verkey) => {
         if (!param.info.timestamp) {
-            logger.info('[authorization.js] param.info.timestamp is not exist')
+            logger.info('[auth_service.js] param.info.timestamp is not exist')
             return {
                 success : false,
                 message : 'param.info.timestamp is not exist',
@@ -12,7 +12,7 @@ module.exports = {
         }
 
         if (Math.abs(new Date(param.info.timestamp) - new Date()) > 300000) {
-            logger.info('[authorization.js] time synchronization needed')
+            logger.info('[auth_service.js] time synchronization needed')
             return {
                 success : false,
                 message : 'time synchronization needed',
