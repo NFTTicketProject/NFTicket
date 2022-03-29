1b505a2b43c3155ef6a9c122519ea8a233c947e0
김형준 <ninini6630@gmail.com>
2022-03-29 18:32:04 +0900

    Chore: 불필요한 컨트롤러 삭제

--- back/app/app/routes/controllers/root.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.all('/', (req, res) => {
-    res.send(`${req.protocol} ${req.ip} Welcome NodeJS`)
-})
-
-module.exports = router
