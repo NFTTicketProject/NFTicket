1c24177fb6d47e6029c4b4691edd89d03144e8eb
김한길 <nuclear0789@naver.com>
2022-03-31 08:15:28 +0900

    S06P22A102-198 Feature:front: DetailDescription에 소제목 삽입

--- frontend/src/components/TicketDetail/DetailDescription.js
+++ frontend/src/components/TicketDetail/DetailDescription.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-const DetailDescription = () => {
-  const PosterDetail = styled.img``;
+const PosterDetail = styled.img`
+  width: 100%;
+`;
 
-  // src='images/posterImg1.png'
+const TitleText = styled.h2`
+  margin-left: 20px;
+`;
+
+const DetailDescription = () => {
   return (
     <div>
+      <TitleText>상세 정보</TitleText>
       <PosterDetail src='images/AIDA.jpg' alt='poster img'></PosterDetail>
     </div>
   );
