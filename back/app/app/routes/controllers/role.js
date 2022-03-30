f0306ccf789fa4f5aaa6ddb49565d59fece72254
김형준 <ninini6630@gmail.com>
2022-03-30 17:41:05 +0900

    Feature:back: 역할 정보 컨트롤러 EP 변경

--- back/app/app/routes/controllers/role.js
+++ back/app/app/routes/controllers/role.js
@@ -2,6 +2,15 @@ const role_service = require("../../services/role_service");
 const express = require("express");
 const router = express.Router();
 
+// 역할 등록
+router.post("/", async (req, res, err) => {
+  const result = await role_service.createRole(req.body);
+
+  if (!result) res.status(404);
+
+  res.json(result);
+});
+
 // 역할 조회
 router.get("/:saleId", async (req, res, err) => {
   const result = await role_service.getRole(req.params.saleId);
@@ -12,7 +21,7 @@ router.get("/:saleId", async (req, res, err) => {
 });
 
 // 역할명 조회
-router.get("/:saleId", async (req, res, err) => {
+router.get("/:saleId/occupation", async (req, res, err) => {
   const result = await role_service.getOccupation(req.params.saleId);
 
   if (!result) res.status(404);
@@ -21,7 +30,7 @@ router.get("/:saleId", async (req, res, err) => {
 });
 
 // 스태프아이디 조회
-router.get("/:saleId", async (req, res, err) => {
+router.get("/:saleId/staff-id", async (req, res, err) => {
   const result = await role_service.getStaffId(req.params.saleId);
 
   if (!result) res.status(404);
@@ -30,7 +39,7 @@ router.get("/:saleId", async (req, res, err) => {
 });
 
 // 공연아이디 조회
-router.get("/:saleId", async (req, res, err) => {
+router.get("/:saleId/show-id", async (req, res, err) => {
   const result = await role_service.getShowId(req.params.saleId);
 
   if (!result) res.status(404);
@@ -38,18 +47,8 @@ router.get("/:saleId", async (req, res, err) => {
   res.json(result);
 });
 
-
-// 역할 등록
-router.post("/:saleId", async (req, res, err) => {
-  const result = await role_service.createRole(req.body);
-
-  if (!result) res.status(404);
-
-  res.json(result);
-});
-
 // 역할 수정
-router.patch("/edit/:saleId", async (req, res, err) => {
+router.patch("/:saleId", async (req, res, err) => {
   const result = await role_service.setRole(req.body);
 
   if (!result) res.status(404);
@@ -58,7 +57,7 @@ router.patch("/edit/:saleId", async (req, res, err) => {
 });
 
 // 역할명 수정
-router.patch("/edit/:saleId", async (req, res, err) => {
+router.patch("/:saleId/occupation", async (req, res, err) => {
   const result = await role_service.setOccupation(req.body);
 
   if (!result) res.status(404);
@@ -67,7 +66,7 @@ router.patch("/edit/:saleId", async (req, res, err) => {
 });
 
 // 스태프 아이디 수정
-router.patch("/edit/:saleId", async (req, res, err) => {
+router.patch("/:saleId/staff-id", async (req, res, err) => {
   const result = await role_service.setStaffId(req.body);
 
   if (!result) res.status(404);
@@ -76,7 +75,7 @@ router.patch("/edit/:saleId", async (req, res, err) => {
 });
 
 // 공연 아이디 수정
-router.patch("/edit/:saleId", async (req, res, err) => {
+router.patch("/:saleId/show-id", async (req, res, err) => {
   const result = await role_service.setShowId(req.body);
 
   if (!result) res.status(404);
