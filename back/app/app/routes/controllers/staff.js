8aff448ec503e58c7aa579218cdc8df8f69af1ac
김형준 <ninini6630@gmail.com>
2022-03-30 17:42:05 +0900

    Feature:back: 스태프 정보 컨트롤러 EP 변경

--- back/app/app/routes/controllers/staff.js
+++ back/app/app/routes/controllers/staff.js
@@ -3,6 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 // Todo: 서비스 등록 & 호출
+// 스태프 등록
+router.post("/", async (req, res, err) => {
+  const result = await staff_service.createStaff(req.body);
+
+  if (!result) res.status(404);
+
+  res.json(result);
+});
 
 // 스태프 조회
 router.get("/:staffId", async (req, res, err) => {
@@ -14,7 +22,7 @@ router.get("/:staffId", async (req, res, err) => {
 });
 
 // 스태프 이름 조회
-router.get("/name/:staffId", async (req, res, err) => {
+router.get("/:staffId/name", async (req, res, err) => {
   const result = await staff_service.getName(req.params.staffId);
 
   if (!result) res.status(404);
@@ -23,7 +31,7 @@ router.get("/name/:staffId", async (req, res, err) => {
 });
 
 // 스태프 사진 조회
-router.get("/photo/:staffId", async (req, res, err) => {
+router.get("/:staffId/photo", async (req, res, err) => {
   const result = await staff_service.getImageURI(req.params.staffId);
 
   if (!result) res.status(404);
@@ -31,17 +39,8 @@ router.get("/photo/:staffId", async (req, res, err) => {
   res.json(result);
 });
 
-// 스태프 등록
-router.post("/:staffId", async (req, res, err) => {
-  const result = await staff_service.createStaff(req.body);
-
-  if (!result) res.status(404);
-
-  res.json(result);
-});
-
 // 스태프 이름 수정
-router.patch("/edit/name/:staffId", async (req, res, err) => {
+router.patch("/:staffId/name", async (req, res, err) => {
   const result = await staff_service.setName(req.body);
 
   if (!result) res.status(404);
@@ -50,7 +49,7 @@ router.patch("/edit/name/:staffId", async (req, res, err) => {
 });
 
 // 스태프 사진 변경
-router.patch("/edit/photo/:staffId", async (req, res, err) => {
+router.patch("/:staffId/photo", async (req, res, err) => {
   const result = await staff_service.setImageURI(req.body);
 
   if (!result) res.status(404);
