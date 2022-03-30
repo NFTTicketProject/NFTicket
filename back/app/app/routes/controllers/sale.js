ed191308fd71ec15d39167daf2d57aae88494cd3
김형준 <ninini6630@gmail.com>
2022-03-30 17:41:25 +0900

    Feature:back: 판매 정보 컨트롤러 EP 변경

--- back/app/app/routes/controllers/sale.js
+++ back/app/app/routes/controllers/sale.js
@@ -2,6 +2,15 @@ const sale_service = require("../../services/sale_service");
 const express = require("express");
 const router = express.Router();
 
+// 판매글 등록
+router.post("/", async (req, res, err) => {
+    const result = await sale_service.createSale(req.body);
+  
+    if (!result) res.status(404);
+  
+    res.json(result);
+  });
+
 // 판매글 조회
 router.get("/:saleId", async (req, res, err) => {
   const result = await sale_service.getSale(req.params.saleId);
@@ -12,7 +21,7 @@ router.get("/:saleId", async (req, res, err) => {
 });
 
 // 공연 스케줄 아이디 조회
-router.get("/showscheduleid/:saleId", async (req, res, err) => {
+router.get("/:saleId/show-schedule-id", async (req, res, err) => {
   const result = await sale_service.getShowScheduleId(req.params.saleId);
 
   if (!result) res.status(404);
@@ -21,7 +30,7 @@ router.get("/showscheduleid/:saleId", async (req, res, err) => {
 });
 
 // 판매글 설명 조회
-router.get("/description/:saleId", async (req, res, err) => {
+router.get("/:saleId/description", async (req, res, err) => {
   const result = await sale_service.getDescription(req.params.saleId);
 
   if (!result) res.status(404);
@@ -30,7 +39,7 @@ router.get("/description/:saleId", async (req, res, err) => {
 });
 
 // 판매 시작 시간 조회
-router.get("/startedat/:saleId", async (req, res, err) => {
+router.get("/:saleId/started-at", async (req, res, err) => {
   const result = await sale_service.getStartedAt(req.params.saleId);
 
   if (!result) res.status(404);
@@ -39,7 +48,7 @@ router.get("/startedat/:saleId", async (req, res, err) => {
 });
 
 // 판매 종료 시간 조회
-router.get("/endedat/:saleId", async (req, res, err) => {
+router.get("/:saleId/ended-at", async (req, res, err) => {
   const result = await sale_service.getEndedAt(req.params.saleId);
 
   if (!result) res.status(404);
@@ -47,17 +56,8 @@ router.get("/endedat/:saleId", async (req, res, err) => {
   res.json(result);
 });
 
-// 판매글 등록
-router.post("/:saleId", async (req, res, err) => {
-  const result = await sale_service.createSale(req.body);
-
-  if (!result) res.status(404);
-
-  res.json(result);
-});
-
 // 판매글 수정
-router.patch("/edit/:saleId", async (req, res, err) => {
+router.patch("/:saleId", async (req, res, err) => {
   const result = await sale_service.setSale(req.body);
 
   if (!result) res.status(404);
@@ -66,7 +66,7 @@ router.patch("/edit/:saleId", async (req, res, err) => {
 });
 
 // 공연 스케줄 아이디 변경
-router.patch("/edit/showscheduleid/:saleId", async (req, res, err) => {
+router.patch("/:saleId/show-schedule-id", async (req, res, err) => {
   const result = await sale_service.setShowScheduleId(req.body);
 
   if (!result) res.status(404);
@@ -75,7 +75,7 @@ router.patch("/edit/showscheduleid/:saleId", async (req, res, err) => {
 });
 
 // 판매글 설명 변경
-router.patch("/edit/description/:saleId", async (req, res, err) => {
+router.patch("/:saleId/description", async (req, res, err) => {
   const result = await sale_service.setDescription(req.body);
 
   if (!result) res.status(404);
@@ -84,7 +84,7 @@ router.patch("/edit/description/:saleId", async (req, res, err) => {
 });
 
 // 판매 시작 시간 변경
-router.patch("/edit/startedat/:saleId", async (req, res, err) => {
+router.patch("/:saleId/started-at", async (req, res, err) => {
   const result = await sale_service.setStartedAt(req.body);
 
   if (!result) res.status(404);
@@ -93,7 +93,7 @@ router.patch("/edit/startedat/:saleId", async (req, res, err) => {
 });
 
 // 판매 종료 시간 변경
-router.patch("/edit/endedat/:saleId", async (req, res, err) => {
+router.patch("/:saleId/endedat", async (req, res, err) => {
   const result = await sale_service.setEndedAt(req.body);
 
   if (!result) res.status(404);
