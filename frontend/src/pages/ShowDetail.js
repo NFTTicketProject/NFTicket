e9e91152b968fa49751cb12563e209fa6ad54479
ruwan9 <ruwan9@kakao.com>
2022-03-31 17:55:25 +0900

    S06P22A102-189 Feature:front schedule detail페이지 작업

--- frontend/src/pages/ShowDetail.js
+++ frontend/src/pages/ShowDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { web3, showScheduleAbi, myTicketContract } from "../utils/web3";
 
-function TC() {
+function ShowDetail() {
   const { showScheduleAddress } = useParams();
   const showScheduleContract = new web3.eth.Contract(showScheduleAbi, showScheduleAddress);
   // const [showId, setShowId] = useState();
@@ -10,6 +10,7 @@ function TC() {
   const [ticketDetail, setTicketDetail] = useState([]);
   const userData = JSON.parse(localStorage.getItem("userAccount"));
 
+  // contract 통해서 show detail 정보 가져오기
   const callShowDetail = async () => {
     try {
       const showId = await showScheduleContract.methods.getShowId().call();
@@ -17,7 +18,9 @@ function TC() {
       const ticketClassCount = await showScheduleContract.methods.getTicketClassCount().call();
       const resellPolicy = await showScheduleContract.methods.getResellPolicy().call();
       const maxMintCount = await showScheduleContract.methods.getMaxMintCount().call();
-      console.log(maxMintCount);
+      const isCancelled = await showScheduleContract.methods.isCancelled().call();
+      window.localStorage.setItem("isCancelled", isCancelled);
+      // console.log(maxMintCount);
       const tmp = [];
       for (let i = 0; i < ticketClassCount; i++) {
         const ticketClassName = await showScheduleContract.methods.getTicketClassName(i).call();
@@ -36,6 +39,7 @@ function TC() {
         stageName,
         ticketClassCount,
         maxMintCount,
+        isCancelled,
         isResellAvailable: resellPolicy[0],
         resellRoyaltyRatePercent: resellPolicy[1],
         resellPriceLimit: resellPolicy[2],
@@ -45,20 +49,26 @@ function TC() {
     }
   };
 
+  // show 등록 취소 버튼
   const cancelShow = async () => {
     try {
       const cancel = await showScheduleContract.methods.cancel().send({ from: userData.account });
       console.log(cancel);
+      if (cancel.status) {
+        window.localStorage.setItem(`${showScheduleAddress}Cancelled`, true);
+      }
     } catch (err) {
       console.error(err);
     }
   };
 
+  // 돈 가져오기
   const onWithdraw = async () => {
     try {
       const withdraw = await showScheduleContract.methods
         .withdraw()
         .send({ from: userData.account });
+      console.log(withdraw);
     } catch (err) {
       console.error(err);
     }
@@ -78,15 +88,15 @@ function TC() {
       <div>maxMintCount = {showDetail.maxMintCount}</div>
       {showDetail.isResellAvailable ? (
         <div>
-          <div>{showDetail.resellRoyaltyRatePercent}</div>
-          <div>{showDetail.resellPriceLimit}</div>
+          <div>resellRoyaltyRatePercent = {showDetail.resellRoyaltyRatePercent}</div>
+          <div>resellPriceLimit = {showDetail.resellPriceLimit}</div>
         </div>
       ) : (
         <div></div>
       )}
       <hr />
       {ticketDetail.map((it, idx) => (
-        <div>
+        <div key={idx}>
           <div>ticketClassName = {it.ticketClassName}</div>
           <div>ticketClassPrice = {it.ticketClassPrice}</div>
           <div>ticketClassMaxMintCount = {it.ticketClassMaxMintCount}</div>
@@ -94,7 +104,7 @@ function TC() {
         </div>
       ))}
       <div>
-        <button onClick={cancelShow}>cancel Show</button>
+        <button onClick={cancelShow}>Cancel Show</button>
       </div>
       <div>
         <button onClick={onWithdraw}>Withdraw</button>
@@ -103,4 +113,4 @@ function TC() {
   );
 }
 
-export default TC;
+export default ShowDetail;
