c3724ade572d1b915d16c162f51c0beee5eb6154
김한길 <nuclear0789@naver.com>
2022-04-01 17:33:47 +0900

    S06P22A102-202 Feature:Front: Top 컴포넌트 제작

--- frontend/src/components/TicketDetailHandover/Top.js
+++ frontend/src/components/TicketDetailHandover/Top.js
@@ -4,11 +4,35 @@ import styled from "styled-components";
 import TopLeft from "./TopLeft";
 import TopRight from "./TopRight";
 
+const TopCss = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 500px;
+  margin-top: 50px;
+`;
+
+const TopLeftCss = styled.div`
+  width: 400px;
+  background-color: #fdebd0;
+`;
+
+const TopRightCss = styled.div`
+  width: 600px;
+  background-color: #bfc9ca;
+`;
+
 const Top = () => {
   return (
     <div>
-      <TopLeft></TopLeft>
-      <TopRight></TopRight>
+      <TopCss>
+        <TopLeftCss>
+          <TopLeft></TopLeft>
+        </TopLeftCss>
+
+        <TopRightCss>
+          <TopRight></TopRight>
+        </TopRightCss>
+      </TopCss>
     </div>
   );
 };
