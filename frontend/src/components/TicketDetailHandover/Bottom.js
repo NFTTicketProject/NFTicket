a39b9a9f98334ceccd9ee0a4c56a9dd8961f6543
김한길 <nuclear0789@naver.com>
2022-04-01 17:32:24 +0900

    Feature:front: 양도페이지 Bottom 컴포넌트 제작

--- frontend/src/components/TicketDetailHandover/Bottom.js
+++ frontend/src/components/TicketDetailHandover/Bottom.js
@@ -1,7 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+
+import Notification from "../TicketDetail/Notification";
+import TradeHistory from "../TicketDetail/TradeHistory";
+import NFTInfo from "../TicketDetail/NFTInfo";
+import OtherTicketInfo from "./OtherTicketInfo";
+
+const NotificationCss = styled.div`
+  margin-top: 20px;
+`;
+
+const TradeHistoryCss = styled.div``;
+
+const NFTInfoCss = styled.div`
+  width: 500px;
+`;
+
+const OtherTicketInfoCss = styled.div`
+  background-color: #d35400;
+`;
 
 const Bottom = () => {
-  return <div>Bottom</div>;
+  return (
+    <div>
+      <NotificationCss>
+        <Notification></Notification>
+      </NotificationCss>
+
+      <TradeHistoryCss>
+        <TradeHistory></TradeHistory>
+      </TradeHistoryCss>
+
+      <NFTInfoCss>
+        <NFTInfo></NFTInfo>
+      </NFTInfoCss>
+      <OtherTicketInfoCss>
+        <OtherTicketInfo></OtherTicketInfo>
+      </OtherTicketInfoCss>
+    </div>
+  );
 };
 
 export default Bottom;
