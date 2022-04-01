7796e232f90154059e16faf7b37027d2660abf2b
김형준 <ninini6630@gmail.com>
2022-04-01 12:31:51 +0900

    Refactor: 변수명 정리 및 예외 처리 (HTTP Status Code 추가)

--- back/app/app/routes/controllers/account.js
+++ back/app/app/routes/controllers/account.js
@@ -60,12 +60,14 @@ router.post('/:wallet_address', async (req, res) => {
         {
             status_code = 400
             result = {message: "Invalid address"}
+            return
         }
 
         result = await profile.getProfile(req.params.wallet_address)
         if (result)
         {
             status_code = 200
+            return
         }
         else
         {
@@ -79,13 +81,14 @@ router.post('/:wallet_address', async (req, res) => {
     
             result = await profile.createProfile(newInfo)
             status_code = 201
+            return
         }
     } catch (e) {
-        logger.error('[Controller] POST /:wallet_address ::: ' + e);
+        logger.error('[Controller] account ::: POST /:wallet_address ::: ' + e);
     } finally {
-        logger.info('[Controller] POST /:wallet_address ::: ' + JSON.stringify(result));
+        logger.info('[Controller] account ::: POST /:wallet_address ::: ' + JSON.stringify(result));
         res.status(status_code)
-        res.send(result)
+        res.json(result)
     }
 })
 
@@ -424,106 +427,216 @@ router.patch('/edit/gallery/:walletId', async (req, res) => {
     res.send({message: "This API will be deprecated. Find out more at our swagger docs."})
 })
 
-router.patch('/:walletId', async (req, res) => {
-    const newInfo = {
-        wallet_id: req.params.walletId,
-        nickname: req.body.info.nickname,
-        description: req.body.info.description,
-        image_uri: req.body.info.image_uri,
+router.patch('/:wallet_address', async (req, res) => {
+    let status_code = 500;
+    let result;
+
+    try {
+        if (Object.keys(req.body).length == 0 || !req.body.info || !req.body.info.timestamp || !req.body.hash_sign) {
+            status_code = 400
+            result = { message: "Invaild request" }
+            return
+        }
+
+        const newInfo = {
+            wallet_id: req.params.wallet_address,
+            ...req.body.info
+        }
+        
+        if (newInfo['nickname']) newInfo['nickname'] = newInfo['nickname'].slice(0,10)
+    
+        const validation = await auth.ownerCheck(req.body, req.params.wallet_address)
+    
+        if (!validation.success) {
+            status_code = 403
+            result = { message: validation.message }
+            return
+        }
+
+        result = await profile.getProfile(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = await profile.setProfile(newInfo)
+        result = {}
+    } catch (e) {
+        logger.error('[Controller] account ::: PATCH /:wallet_address ::: ' + e);
+    } finally {
+        logger.info('[Controller] account ::: PATCH /:wallet_address ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
     }
-
-    const validation = await auth.ownerCheck(req.body, req.params.walletId)
-
-    if (!validation.success) {
-        res.status(500)
-        res.send({message : validation.message})
-        return;
-    }
-
-    const status = await profile.setProfile(newInfo)
-
-    res.status(status)
-    res.send()
 })
 
-router.patch('/:walletId/nickname', async (req, res) => {
-    const newInfo = {
-        wallet_id: req.params.walletId,
-        nickname: req.body.info.nickname.slice(0, 10),
+router.patch('/:wallet_address/nickname', async (req, res) => {
+    let status_code = 500;
+    let result;
+
+    try {
+        if (Object.keys(req.body).length == 0 || !req.body.info || !req.body.info.timestamp || !req.body.hash_sign || !req.body.info.nickname) {
+            status_code = 400
+            result = { message: "Invaild request" }
+            return
+        }
+
+        const newInfo = {
+            wallet_id: req.params.wallet_address,
+            ...(req.body.info.nickname && { nickname: req.body.info.nickname.slice(0,10) })
+        }
+            
+        const validation = await auth.ownerCheck(req.body, req.params.wallet_address)
+    
+        if (!validation.success) {
+            status_code = 403
+            result = { message: validation.message }
+            return
+        }
+
+        result = await profile.getProfile(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = await profile.setNickname(newInfo)
+        result = {}
+    } catch (e) {
+        logger.error('[Controller] account ::: PATCH /:wallet_address/nickname ::: ' + e);
+    } finally {
+        logger.info('[Controller] account ::: PATCH /:wallet_address/nickname ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
     }
-
-    const validation = await auth.ownerCheck(req.body, req.params.walletId)
-
-    if (!validation.success) {
-        res.status(500)
-        res.send({message : validation.message})
-        return;
-    }
-
-    const status = await profile.setNickname(newInfo)
-
-    res.status(status)
-    res.send()
 })
 
-router.patch('/:walletId/description', async (req, res) => {
-    const newInfo = {
-        wallet_id: req.params.walletId,
-        description: req.body.info.description,
+router.patch('/:wallet_address/description', async (req, res) => {
+    let status_code = 500;
+    let result;
+
+    try {
+        if (Object.keys(req.body).length == 0 || !req.body.info || !req.body.info.timestamp || !req.body.hash_sign || !req.body.info.description) {
+            status_code = 400
+            result = { message: "Invaild request" }
+            return
+        }
+
+        const newInfo = {
+            wallet_id: req.params.wallet_address,
+            ...(req.body.info.description && { description: req.body.info.description })
+        }
+            
+        const validation = await auth.ownerCheck(req.body, req.params.wallet_address)
+    
+        if (!validation.success) {
+            status_code = 403
+            result = { message: validation.message }
+            return
+        }
+
+        result = await profile.getProfile(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = await profile.setDescription(newInfo)
+        result = {}
+    } catch (e) {
+        logger.error('[Controller] account ::: PATCH /:wallet_address/description ::: ' + e);
+    } finally {
+        logger.info('[Controller] account ::: PATCH /:wallet_address/description ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
     }
-
-    const validation = await auth.ownerCheck(req.body, req.params.walletId)
-
-    if (!validation.success) {
-        res.status(500)
-        res.send({message : validation.message})
-        return;
-    }
-
-    const status = await profile.setDescription(newInfo)
-
-    res.status(status)
-    res.send()
 })
 
-router.patch('/:walletId/image-uri', async (req, res) => {
-    const newInfo = {
-        wallet_id: req.params.walletId,
-        image_uri: req.body.info.image_uri,
+router.patch('/:wallet_address/image-uri', async (req, res) => {
+    let status_code = 500;
+    let result;
+
+    try {
+        if (Object.keys(req.body).length == 0 || !req.body.info || !req.body.info.timestamp || !req.body.hash_sign || !req.body.info.image_uri) {
+            status_code = 400
+            result = { message: "Invaild request" }
+            return
+        }
+
+        const newInfo = {
+            wallet_id: req.params.wallet_address,
+            ...(req.body.info.image_uri && { image_uri: req.body.info.image_uri })
+        }
+            
+        const validation = await auth.ownerCheck(req.body, req.params.wallet_address)
+    
+        if (!validation.success) {
+            status_code = 403
+            result = { message: validation.message }
+            return
+        }
+
+        result = await profile.getProfile(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = await profile.setImageURI(newInfo)
+        result = {}
+    } catch (e) {
+        logger.error('[Controller] account ::: PATCH /:wallet_address/image-uri ::: ' + e);
+    } finally {
+        logger.info('[Controller] account ::: PATCH /:wallet_address/image-uri ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
     }
-
-    const validation = await auth.ownerCheck(req.body, req.params.walletId)
-
-    if (!validation.success) {
-        res.status(500)
-        res.send({message : validation.message})
-        return;
-    }
-
-    const status = await profile.setImageURI(newInfo)
-
-    res.status(status)
-    res.send()
 })
 
-router.patch('/:walletId/gallery', async (req, res) => {
-    const newInfo = {
-        wallet_id: req.params.walletId,
-        gallery: req.body.info.gallery,
+router.patch('/:wallet_address/gallery', async (req, res) => {
+    let status_code = 500;
+    let result;
+
+    try {
+        if (Object.keys(req.body).length == 0 || !req.body.info || !req.body.info.timestamp || !req.body.hash_sign || !req.body.info.gallery) {
+            status_code = 400
+            result = { message: "Invaild request" }
+            return
+        }
+
+        const newInfo = {
+            wallet_id: req.params.wallet_address,
+            ...(req.body.info.gallery && { image_uri: req.body.info.gallery })
+        }
+            
+        const validation = await auth.ownerCheck(req.body, req.params.wallet_address)
+    
+        if (!validation.success) {
+            status_code = 403
+            result = { message: validation.message }
+            return
+        }
+
+        result = await profile.getProfile(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = await profile.setGallery(newInfo)
+        result = {}
+    } catch (e) {
+        logger.error('[Controller] account ::: PATCH /:wallet_address/gallery ::: ' + e);
+    } finally {
+        logger.info('[Controller] account ::: PATCH /:wallet_address/gallery ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
     }
-
-    const validation = await auth.ownerCheck(req.body, req.params.walletId)
-
-    if (!validation.success) {
-        res.status(500)
-        res.send({message : validation.message})
-        return;
-    }
-
-    const status = await profile.setGallery(newInfo)
-
-    res.status(status)
-    res.send()
 })
 
 module.exports = router
