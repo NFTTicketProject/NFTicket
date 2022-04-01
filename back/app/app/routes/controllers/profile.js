bbd6d69c8effb3375a688eeb2104bc67994c6477
김형준 <ninini6630@gmail.com>
2022-04-01 12:31:51 +0900

    Refactor: 변수명 정리 및 예외 처리 (HTTP Status Code 추가)

--- back/app/app/routes/controllers/profile.js
+++ back/app/app/routes/controllers/profile.js
@@ -1,4 +1,5 @@
 const profile = require("../../services/profile")
+const { logger } = require('../../utils/winston')
 const express = require('express')
 const router = express.Router()
 
@@ -24,13 +25,26 @@ const router = express.Router()
  *        description: 존재하지 않는 사용자
  */
 // 프로필 전체 조회
-router.get('/:walletId', async (req, res, err) => {
-    const result = await profile.getProfile(req.params.walletId);
+router.get('/:wallet_address', async (req, res) => {
+    let status_code = 500;
+    let result;
 
-    if (!result)
-        res.status(404)
-
-    res.json(result)
+    try {
+        result = await profile.getProfile(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = 200
+    } catch (e) {
+        logger.error('[Controller] profile ::: GET /:wallet_address ::: ' + e);
+    } finally {
+        logger.info('[Controller] profile ::: GET /:wallet_address ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
+    }
 })
 
 /**
@@ -197,58 +211,118 @@ router.get('/gallery/:walletId', async (req, res, err) => {
 
 
 // 닉네임 조회
-router.get('/:walletId/nickname', async (req, res, err) => {
-    // walletID 와 일치하는 닉네임 반환
-    const result = await profile.getNickname(req.params.walletId)
+router.get('/:wallet_address/nickname', async (req, res) => {
+    let status_code = 500;
+    let result;
 
-    if (!result)
-        res.status(404)
-
-    res.json(result)
+    try {
+        result = await profile.getNickname(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = 200
+    } catch (e) {
+        logger.error('[Controller] profile ::: GET /:wallet_address/nickname ::: ' + e);
+    } finally {
+        logger.info('[Controller] profile ::: GET /:wallet_address/nickname ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
+    }
 })
 
 // 자기소개 조회
-router.get('/:walletId/description', async (req, res, err) => {
-    // walletID 와 일치하는 자기소개 반환
-    const result = await profile.getDescription(req.params.walletId)
+router.get('/:wallet_address/description', async (req, res) => {
+    let status_code = 500;
+    let result;
 
-    if (!result)
-        res.status(404)
-
-    res.json(result)
+    try {
+        result = await profile.getDescription(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = 200
+    } catch (e) {
+        logger.error('[Controller] profile ::: GET /:wallet_address/description ::: ' + e);
+    } finally {
+        logger.info('[Controller] profile ::: GET /:wallet_address/description ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
+    }
 })
 
 // 사용자 이미지 조회
-router.get('/:walletId/image-uri', async (req, res, err) => {
-    // walletID 와 일치하는 자기소개 반환
-    const result = await profile.getImageURI(req.params.walletId)
+router.get('/:wallet_address/image-uri', async (req, res) => {
+    let status_code = 500;
+    let result;
 
-    if (!result)
-        res.status(404)
-
-    res.json(result)
+    try {
+        result = await profile.getImageURI(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = 200
+    } catch (e) {
+        logger.error('[Controller] profile ::: GET /:wallet_address/image-uri ::: ' + e);
+    } finally {
+        logger.info('[Controller] profile ::: GET /:wallet_address/image-uri ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
+    }
 })
 
 // 가입일 조회
-router.get('/:walletId/created-at', async (req, res, err) => {
-    // walletID 와 일치하는 가입일 반환
-    const result = await profile.getCreatedAt(req.params.walletId)
+router.get('/:wallet_address/created-at', async (req, res) => {
+    let status_code = 500;
+    let result;
 
-    if (!result)
-        res.status(404)
-
-    res.json(result)
+    try {
+        result = await profile.getCreatedAt(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = 200
+    } catch (e) {
+        logger.error('[Controller] profile ::: GET /:wallet_address/created-at ::: ' + e);
+    } finally {
+        logger.info('[Controller] profile ::: GET /:wallet_address/created-at ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
+    }
 })
 
 // 사용자 갤러리 사이즈 조회
-router.get('/:walletId/gallery', async (req, res, err) => {
-    // walletID 와 일치하는 갤러리 사이즈 반환
-    const result = await profile.getGallery(req.params.walletId)
+router.get('/:wallet_address/gallery', async (req, res) => {
+    let status_code = 500;
+    let result;
 
-    if (!result)
-        res.status(404)
-
-    res.json(result)
+    try {
+        result = await profile.getGallery(req.params.wallet_address);
+        if (!result) {
+            status_code = 404
+            result = { message: "Address doesn't exist" }
+            return
+        }
+            
+        status_code = 200
+    } catch (e) {
+        logger.error('[Controller] profile ::: GET /:wallet_address/gallery ::: ' + e);
+    } finally {
+        logger.info('[Controller] profile ::: GET /:wallet_address/gallery ::: ' + JSON.stringify(result));
+        res.status(status_code)
+        res.json(result)
+    }
 })
 
 module.exports = router
