696f1257479b22ed8aaafbc1e31312a3b50ade46
선민기 <suns1502@gmail.com>
2022-04-01 21:42:19 +0900

    Fix:back: WebServer 리팩토링

--- back/app/app/routes/controllers/role.js
+++ back/app/app/routes/controllers/role.js
@@ -1,86 +1,76 @@
 const role = require("../../services/role");
 const express = require("express");
+const {logger} = require("../../utils/winston");
 const router = express.Router();
 
 // 역할 등록
 router.post("/", async (req, res, err) => {
-  const result = await role.createRole(req.body);
+  let status_code = 500;
+  let result;
 
-  if (!result) res.status(404);
+  try {
+    result = await role.createRole(req.body)
+    if (!result)
+    {
+      status_code = 400
+      result = { message: "Invalid request" }
+      return
+    }
 
-  res.json(result);
+    status_code = 201
+  } catch (e) {
+    logger.error('[Controller] role ::: POST / ::: ' + e);
+  } finally {
+    logger.info('[Controller] role ::: POST / ::: ' + JSON.stringify(result));
+    res.status(status_code)
+    res.json(result)
+  }
 });
 
 // 역할 조회
-router.get("/:saleId", async (req, res, err) => {
-  const result = await role.getRole(req.params.saleId);
+router.get("/:showId/:staffId", async (req, res, err) => {
+  let status_code = 500;
+  let result;
 
-  if (!result) res.status(404);
+  try {
+    result = await role.getRole(req.params.showId, req.params.staffId);
+    if (!result) {
+      status_code = 404
+      result = {message: "Role doesn't exist"}
+      return
+    }
 
-  res.json(result);
-});
-
-// 역할명 조회
-router.get("/:saleId/occupation", async (req, res, err) => {
-  const result = await role.getOccupation(req.params.saleId);
-
-  if (!result) res.status(404);
-
-  res.json(result);
-});
-
-// 스태프아이디 조회
-router.get("/:saleId/staff-id", async (req, res, err) => {
-  const result = await role.getStaffId(req.params.saleId);
-
-  if (!result) res.status(404);
-
-  res.json(result);
-});
-
-// 공연아이디 조회
-router.get("/:saleId/show-id", async (req, res, err) => {
-  const result = await role.getShowId(req.params.saleId);
-
-  if (!result) res.status(404);
-
-  res.json(result);
+    status_code = 200
+  } catch (e) {
+    logger.error('[Controller] role ::: GET /:showId/:staffId ::: ' + e);
+  } finally {
+    logger.info('[Controller] role ::: GET /:showId/:staffId ::: ' + JSON.stringify(result));
+    res.status(status_code)
+    res.json(result)
+  }
 });
 
 // 역할 수정
-router.patch("/:saleId", async (req, res, err) => {
-  const result = await role.setRole(req.body);
+router.patch("/:show_id/:staff_id", async (req, res, err) => {
+  let status_code = 500;
+  let result;
 
-  if (!result) res.status(404);
+  try {
+    if (Object.keys(req.body).length == 0) {
+      status_code = 400
+      result = {message: "Invaild request"}
+      return
+    }
 
-  res.json(result);
-});
-
-// 역할명 수정
-router.patch("/:saleId/occupation", async (req, res, err) => {
-  const result = await role.setOccupation(req.body);
-
-  if (!result) res.status(404);
-
-  res.json(result);
-});
-
-// 스태프 아이디 수정
-router.patch("/:saleId/staff-id", async (req, res, err) => {
-  const result = await role.setStaffId(req.body);
-
-  if (!result) res.status(404);
-
-  res.json(result);
-});
-
-// 공연 아이디 수정
-router.patch("/:saleId/show-id", async (req, res, err) => {
-  const result = await role.setShowId(req.body);
-
-  if (!result) res.status(404);
-
-  res.json(result);
+    status_code = await role.setRole({ ...req.params, ...req.body })
+    result = {}
+  } catch (e) {
+    logger.error('[Controller] role ::: PATCH /:showId/:staffId ::: ' + e);
+  } finally {
+    logger.info('[Controller] role ::: PATCH /:showId/:staffId ::: ' + JSON.stringify(result));
+    res.status(status_code)
+    res.json(result)
+  }
 });
 
 module.exports = router;
--- back/app/app/services/profile.js
+++ back/app/app/services/profile.js
@@ -1,4 +1,3 @@
-const text_generater = require('./text_generater')
 const prisma = require("../utils/prisma")
 const { logger } = require('../utils/winston')
 
@@ -92,6 +91,20 @@ module.exports = {
         logger.info('[Service] profile ::: getImageURI ::: ' + JSON.stringify(result))
 
         return result
+    },
+    getRandomNickname : async () => {
+        try {
+            const adjective = await prisma.$queryRaw`SELECT adjective FROM RandomAdjective order by rand() limit 1`
+            const noun = await prisma.$queryRaw`SELECT noun FROM RandomNoun order by rand() limit 1`
+
+            logger.error('[Service] text_generater ::: getRandomNickname ::: ' + `${adjective[0].adjective} ${noun[0].noun}`)
+
+            return `${adjective[0].adjective} ${noun[0].noun}`
+        }catch (e) {
+            logger.error('[Service] text_generater ::: getRandomNickname ::: ' + e)
+
+            return '닉네임생성실패'
+        }
     },
     setProfile : async (info) =>{
         try {
