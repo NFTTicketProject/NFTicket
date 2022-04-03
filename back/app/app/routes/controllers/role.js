fba62121bb00f4b585d040056c74f788fc9fad04
김형준 <ninini6630@gmail.com>
2022-04-03 13:08:38 +0900

    Feature:back: 역할 정보 전체 조회 API 추가

--- back/app/app/routes/controllers/role.js
+++ back/app/app/routes/controllers/role.js
@@ -43,6 +43,38 @@ router.post("/", async (req, res, err) =>
   }
 })
 
+// 역할 전체 조회
+router.get("/", async (req, res, err) =>
+{
+  let status_code = 500
+  let result
+
+  try
+  {
+    result = await role.getAllRole()
+    if (!result)
+    {
+      status_code = 404
+      result = { message: `${ controller_name } doesn't exist` }
+      return
+    }
+
+    status_code = 200
+  } catch (e)
+  {
+    logger.error(
+      `[Controller] ${ controller_name } ::: ${ req.method } ${ req.path } ::: ${ JSON.stringify(result) } ::: ${ e }`
+    )
+  } finally
+  {
+    logger.info(
+      `[Controller] ${ controller_name } ::: ${ req.method } ${ req.path } ::: ${ JSON.stringify(result) }`
+    )
+    res.status(status_code)
+    res.json(result)
+  }
+})
+
 // 역할 조회
 router.get("/:show_id/:staff_id", async (req, res, err) =>
 {
