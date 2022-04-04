b6c455d4043a4b614f798c9f0f065415445ed496
김한길 <nuclear0789@naver.com>
2022-04-05 08:32:49 +0900

    Fix:front: Footer 모든 페이지에 적용

--- frontend/src/pages/Guide.js
+++ frontend/src/pages/Guide.js
@@ -1,31 +1,16 @@
 import React from "react";
-import styled from "styled-components";
 
 import GuideBody from "../components/guide/GuideBody";
 import GuideHeader from "../components/guide/GuideHeader";
 import GuideTitle from "../components/guide/GuideTitle";
-import Footer from "../components/Footer";
-
-const TotalWrapDiv = styled.div`
-  position: relative;
-  min-height: 100vh;
-`;
-
-const ContentWrapDiv = styled.div`
-  padding-bottom: 16rem;
-`;
 
 const Guide = () => {
   return (
-    <TotalWrapDiv>
-      <ContentWrapDiv>
-        <GuideHeader></GuideHeader>
-        <GuideTitle></GuideTitle>
-        <GuideBody></GuideBody>
-      </ContentWrapDiv>
-
-      <Footer></Footer>
-    </TotalWrapDiv>
+    <div>
+      <GuideHeader></GuideHeader>
+      <GuideTitle></GuideTitle>
+      <GuideBody></GuideBody>
+    </div>
   );
 };
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,30 +8,16 @@ import TodayPerformance from "../components/Home/TodayPerformance";
 import SpecialTicket from "../components/Home/SpecialTicket";
 import Ticketo from "../components/Home/Ticketo";
 // import Community from "../components/Home/Community";
-import Footer from "../components/Footer";
-
-const TotalWrapDiv = styled.div`
-  position: relative;
-  min-height: 100vh;
-`;
-
-const ContentWrapDiv = styled.div`
-  padding-bottom: 16rem;
-`;
 
 const Home = () => {
   return (
-    <TotalWrapDiv>
-      <ContentWrapDiv>
-        <HomeSlider></HomeSlider>
-        <TodayPerformance></TodayPerformance>
-        <SpecialTicket></SpecialTicket>
-        <Ticketo></Ticketo>
-        {/* <Community></Community> */}
-
-        <Footer />
-      </ContentWrapDiv>
-    </TotalWrapDiv>
+    <div>
+      <HomeSlider></HomeSlider>
+      <TodayPerformance></TodayPerformance>
+      <SpecialTicket></SpecialTicket>
+      <Ticketo></Ticketo>
+      {/* <Community></Community> */}
+    </div>
   );
 };
 
