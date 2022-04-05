edfbbb17479f147f549b0cba913ea6b7543ba41b
김한길 <nuclear0789@naver.com>
2022-04-06 08:52:15 +0900

    S06P22A102-197 #comment Fix:front: 티켓거래시장 페이지 디자인

--- frontend/src/pages/Market.js
+++ frontend/src/pages/Market.js
@@ -1,12 +1,14 @@
 /* eslint-disable */
 import { Autocomplete, Grid, TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 import PerformTicket from "../components/Home/PerformTicket"; // 임시
 
 import axios from "axios";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
 
 import {
   web3,
@@ -16,6 +18,37 @@ import {
   ticketSaleAbi,
   myTicketContract,
 } from "../utils/web3Config";
+
+const TotalWidthSetting = styled.div`
+  width: 1400px;
+  margin: auto;
+`;
+
+const UpperTitleArea = styled.div`
+  margin: 40px;
+  font-size: 36px;
+  font-weight: bold;
+  margin-left: 50px;
+  margin-bottom: 50px;
+`;
+
+const TotalWrapJustifyCenter = styled.div`
+  display: flex;
+  justify-content: left;
+`;
+
+const SearchBarCategoryArea = styled.div`
+  width: 400px;
+`;
+
+const CategoryBarDiv = styled.div`
+  margin: 20px 0 0 38px;
+  width: 300px;
+`;
+
+const TicketListArea = styled.div`
+  width: 800px;
+`;
 
 const Market = () => {
   const [category, SetCategory] = useState("전체");
@@ -30,37 +63,65 @@ const Market = () => {
       console.log(cnt);
       const ticketInfos = [];
       for (let i = 1; i < parseInt(cnt) + 1; i++) {
-        const saleAddr = await ticketSaleManagerContract.methods.getSaleOfTicket(i).call();
+        const saleAddr = await ticketSaleManagerContract.methods
+          .getSaleOfTicket(i)
+          .call();
         if (saleAddr != "0x0000000000000000000000000000000000000000") {
-          const showScheduleId = await myTicketContract.methods.getShowScheduleId(i).call();
+          const showScheduleId = await myTicketContract.methods
+            .getShowScheduleId(i)
+            .call();
           const showScheduleAddress = await showScheduleManagerContract.methods
             .getShowSchedule(showScheduleId)
             .call();
-          const showScheduleContract = new web3.eth.Contract(showScheduleAbi, showScheduleAddress);
+          const showScheduleContract = new web3.eth.Contract(
+            showScheduleAbi,
+            showScheduleAddress,
+          );
           const showId = await showScheduleContract.methods.getShowId().call();
 
           // 공연 발매자
-          const stageSeller = await showScheduleManagerContract.methods.ownerOf(i).call();
+          const stageSeller = await showScheduleManagerContract.methods
+            .ownerOf(i)
+            .call();
           var stageSellerName = await getUserNickname(stageSeller);
           // 티켓 소유자
-          const ticketSeller = await ticketSaleManagerContract.methods.owner().call();
+          const ticketSeller = await ticketSaleManagerContract.methods
+            .owner()
+            .call();
 
           var ticketSellerName = await getUserNickname(ticketSeller);
-          const showInfo = await axios.get(`https://nfticket.plus/api/v1/show/${showId}`);
+          const showInfo = await axios.get(
+            `https://nfticket.plus/api/v1/show/${showId}`,
+          );
           // console.log(showInfo);
-          const ticketUri = await myTicketContract.methods.getTokenURI(i).call();
+          const ticketUri = await myTicketContract.methods
+            .getTokenURI(i)
+            .call();
           // console.log(ticketUri);
           const categoryName = showInfo.data.category_name;
-          const ticketSaleContract = new web3.eth.Contract(ticketSaleAbi, saleAddr);
+          const ticketSaleContract = new web3.eth.Contract(
+            ticketSaleAbi,
+            saleAddr,
+          );
           const price = await ticketSaleContract.methods.getPrice().call();
-          const startAt = await ticketSaleContract.methods.getStartedAt().call();
+          const startAt = await ticketSaleContract.methods
+            .getStartedAt()
+            .call();
           var dateStart = new Date(startAt * 1000);
           var dateStartString =
-            dateStart.getFullYear() + "." + (dateStart.getMonth() + 1) + "." + dateStart.getDate();
+            dateStart.getFullYear() +
+            "." +
+            (dateStart.getMonth() + 1) +
+            "." +
+            dateStart.getDate();
           const endAt = await ticketSaleContract.methods.getEndedAt().call();
           var dateEnd = new Date(endAt * 1000);
           var dateEndString =
-            dateEnd.getFullYear() + "." + (dateEnd.getMonth() + 1) + "." + dateEnd.getDate();
+            dateEnd.getFullYear() +
+            "." +
+            (dateEnd.getMonth() + 1) +
+            "." +
+            dateEnd.getDate();
 
           var cate = category;
           if (cate === "전체") cate = "";
@@ -96,35 +157,63 @@ const Market = () => {
       // console.log(cnt);
       const ticketInfos = [];
       for (let i = 1; i < parseInt(cnt) + 1; i++) {
-        const saleAddr = await ticketSaleManagerContract.methods.getSaleOfTicket(i).call();
+        const saleAddr = await ticketSaleManagerContract.methods
+          .getSaleOfTicket(i)
+          .call();
         if (saleAddr !== "0x0000000000000000000000000000000000000000") {
-          const showScheduleId = await myTicketContract.methods.getShowScheduleId(i).call();
+          const showScheduleId = await myTicketContract.methods
+            .getShowScheduleId(i)
+            .call();
           const showScheduleAddress = await showScheduleManagerContract.methods
             .getShowSchedule(showScheduleId)
             .call();
-          const showScheduleContract = new web3.eth.Contract(showScheduleAbi, showScheduleAddress);
+          const showScheduleContract = new web3.eth.Contract(
+            showScheduleAbi,
+            showScheduleAddress,
+          );
           const showId = await showScheduleContract.methods.getShowId().call();
           // 공연 발매자
-          const stageSeller = await showScheduleManagerContract.methods.ownerOf(i).call();
+          const stageSeller = await showScheduleManagerContract.methods
+            .ownerOf(i)
+            .call();
 
           var stageSellerName = await getUserNickname(stageSeller);
           // 티켓 소유자
-          const ticketSeller = await ticketSaleManagerContract.methods.owner().call();
+          const ticketSeller = await ticketSaleManagerContract.methods
+            .owner()
+            .call();
 
           var ticketSellerName = await getUserNickname(ticketSeller);
-          const showInfo = await axios.get(`https://nfticket.plus/api/v1/show/${showId}`);
-          const ticketUri = await myTicketContract.methods.getTokenURI(i).call();
+          const showInfo = await axios.get(
+            `https://nfticket.plus/api/v1/show/${showId}`,
+          );
+          const ticketUri = await myTicketContract.methods
+            .getTokenURI(i)
+            .call();
           const categoryName = showInfo.data.category_name;
-          const ticketSaleContract = new web3.eth.Contract(ticketSaleAbi, saleAddr);
+          const ticketSaleContract = new web3.eth.Contract(
+            ticketSaleAbi,
+            saleAddr,
+          );
           const price = await ticketSaleContract.methods.getPrice().call();
-          const startAt = await ticketSaleContract.methods.getStartedAt().call();
+          const startAt = await ticketSaleContract.methods
+            .getStartedAt()
+            .call();
           var dateStart = new Date(startAt * 1000);
           var dateStartString =
-            dateStart.getFullYear() + "." + (dateStart.getMonth() + 1) + "." + dateStart.getDate();
+            dateStart.getFullYear() +
+            "." +
+            (dateStart.getMonth() + 1) +
+            "." +
+            dateStart.getDate();
           const endAt = await ticketSaleContract.methods.getEndedAt().call();
           var dateEnd = new Date(endAt * 1000);
           var dateEndString =
-            dateEnd.getFullYear() + "." + (dateEnd.getMonth() + 1) + "." + dateEnd.getDate();
+            dateEnd.getFullYear() +
+            "." +
+            (dateEnd.getMonth() + 1) +
+            "." +
+            dateEnd.getDate();
 
           var cate = category;
           console.log("카테고리", cate);
@@ -160,7 +249,9 @@ const Market = () => {
 
   const getUserNickname = async (wallet) => {
     try {
-      const response = await axios.get(`https://nfticket.plus/api/v1/profile/nickname/${wallet}`);
+      const response = await axios.get(
+        `https://nfticket.plus/api/v1/profile/nickname/${wallet}`,
+      );
       // console.log(response);
       return response.data.nickname;
     } catch (err) {
@@ -173,7 +264,10 @@ const Market = () => {
     if (keyword) {
       var tmp = [];
       for (let ticket of saleTicketArray) {
-        if (ticket.name.includes(keyword) | ticket.stageSellerName.includes(keyword)) {
+        if (
+          ticket.name.includes(keyword) |
+          ticket.stageSellerName.includes(keyword)
+        ) {
           tmp.push(ticket);
         }
       }
@@ -184,66 +278,64 @@ const Market = () => {
   };
 
   return (
-    <div>
-      <h1 style={{ justifyContent: "center" }}>개인 티켓 거래 시장</h1>
-      <Grid container spacing={2}>
-        <Grid item container spacing={0} xs={2} direction="column">
-          <Grid item container direction="row">
-            <Grid item xs={2}></Grid>
-            <Grid item xs={8}>
-              <TextField
-                id="search"
-                label="크리에이터 또는 제목"
-                variant="standard"
-                onChange={searchKeyword}
-              />
-            </Grid>
-            <Grid item xs={2}>
-              <IconButton type="submit" aria-label="search">
-                <SearchIcon />
-              </IconButton>
-            </Grid>
-          </Grid>
-          <div>
-            <div style={{ padding: "10px" }}>카테고리</div>
+    <TotalWidthSetting>
+      <UpperTitleArea>개인 티켓 거래 시장</UpperTitleArea>
+      <TotalWrapJustifyCenter>
+        <SearchBarCategoryArea>
+          <TextField
+            id='search'
+            label='크리에이터 또는 제목'
+            variant='standard'
+            onChange={searchKeyword}
+            sx={{ ml: 5, width: 300 }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <IconButton type='submit' aria-label='search'>
+                    <SearchIcon style={{ color: "#000000" }} />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <CategoryBarDiv>
             <Autocomplete
               value={category}
-              // onChange={(event, newValue) => {
-              //   SetCategory(newValue);
-              //   getTicketOnSaleCategory(newValue);
-              // }}
-              inputValue={category}
-              onInputChange={(event, newInputValue) => {
-                SetCategory(newInputValue);
-                getTicketOnSaleCategory(newInputValue);
+              onChange={(event, newValue) => {
+                SetCategory(newValue);
+                onSubmitCategory(newValue);
               }}
-              id="controllable-states-demo"
+              id='controllable-states-demo'
               options={categories}
-              renderInput={(params) => <TextField {...params} label="카테고리" />}
-              size="small"
+              renderInput={(params) => (
+                <TextField {...params} label='카테고리' />
+              )}
+              size='small'
             />
-          </div>
-        </Grid>
-        <Grid container xs={10}>
-          {saleTicketSearchArray.map((ticket) => (
-            <Grid item xs={3}>
-              <PerformTicket
-                key={ticket.ticketId}
-                id={ticket.ticketId}
-                ticketUri={ticket.ticketUri}
-                saleAddr={ticket.saleAddr}
-                stageSellerName={ticket.stageSellerName}
-                ticketSellerName={ticket.ticketSellerName}
-                name={ticket.name}
-                price={ticket.price}
-                dateStartString={ticket.dateStartString}
-                dateEndString={ticket.dateEndString}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Grid>
-    </div>
+          </CategoryBarDiv>
+        </SearchBarCategoryArea>
+        <TicketListArea>
+          <Grid container spacing={3} rowSpacing={6}>
+            {saleTicketSearchArray.map((ticket, idx) => (
+              <Grid item xs={4} key={idx}>
+                <PerformTicket
+                  key={ticket.ticketId}
+                  id={ticket.ticketId}
+                  ticketUri={ticket.ticketUri}
+                  saleAddr={ticket.saleAddr}
+                  stageSellerName={ticket.stageSellerName}
+                  ticketSellerName={ticket.ticketSellerName}
+                  name={ticket.name}
+                  price={ticket.price}
+                  dateStartString={ticket.dateStartString}
+                  dateEndString={ticket.dateEndString}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </TicketListArea>
+      </TotalWrapJustifyCenter>
+    </TotalWidthSetting>
   );
 };
 
